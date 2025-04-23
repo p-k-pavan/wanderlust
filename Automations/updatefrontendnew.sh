23d88091b63ff562ae26666ad19f2e54ee5c886e
test <149031026+p-k-pavan@users.noreply.github.com>
2025-04-23 18:10:31 +0530

    Create updatefrontendnew.sh

---
+++ Automations/updatefrontendnew.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+INSTANCE_ID=i-0fb26df9d5fa5fa5f
+
+ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+
+file_to_find="../frontend/.env.docker"
+
+current_url=$(cat $file_to_find)
+
+# Update the .env file if the IP address has changed
+if [[ "$current_url" != "VITE_API_PATH=\"http://${ipv4_address}:5000\"" ]]; then
+    if [ -f $file_to_find ]; then
+        sed -i -e "s|VITE_API_PATH.*|VITE_API_PATH=\"http://${ipv4_address}:5000\"|g" $file_to_find
+    else
+        echo "ERROR: File not found."
+    fi
+f
